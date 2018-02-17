40b39771a601155ad26fe42c2cc44e804de08ce8
Kondzik <kmaslowiec@gmail.com>
2018-02-17 11:34:32 +0100

    version 0.9 Alpha
    *XML completed.
    
    *Intents added

--- app/src/main/java/com/example/android/urbanformcard/MainActivity.java
+++ app/src/main/java/com/example/android/urbanformcard/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.urbanformcard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, getResources().getString(R.string.hello_toast), Toast.LENGTH_SHORT).show();
     }
+
+    public void website(View v){
+
+        Uri webpage = Uri.parse(getResources().getString(R.string.web));
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
+    }
+
+
+    /**
+     * connect with the e-mail app in the device to send the e-mail
+     * @param v
+     */
+
+    public void sentEmail(View v){
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:info@urbanform.eu")); // only email apps should handle this
+
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    /**
+     * connect with the phone app in the device to call the company
+     * @param v
+     */
+
+    public void call(View v){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + getResources().getString(R.string.phone)));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    /**
+     * finds the location on the device's map
+     * @param v
+     */
+
+    public void location(View v){
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=16b+Gdanska+Szczecin+Poland"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+
 }
